b6f5cfcd300c18265d6bd20068d3d57cfea0fcbe
Kappaequiscu <jordiplana89@gmail.com>
2021-08-27 17:19:16 +0200

    App: routing with react-router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from "react"
+import { ThemeProvider } from 'theme-ui'
+import { theme } from "./constants/theme"
+import Search from "./components/Search"
+import Navbar from "./components/Navbar"
+import { BrowserRouter, Route, Switch } from "react-router-dom"
+import Repos from "./components/Repos"
 
 const App = () => ( 
-    <div>
-        <h1>Refreshing</h1>
-    </div>
+    <BrowserRouter>
+        <ThemeProvider theme={theme}>
+            <Navbar image={theme.images.avatar}/>
+            <Switch>
+                <Route path="/repos/:id" component={Repos} />
+                <Route path="/" component={Search} exact />
+            </Switch>
+        </ThemeProvider>
+    </BrowserRouter>
 )
 
 export default App
